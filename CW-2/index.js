3597c35de1ce411c254b66aa876a60cee3804dc0
David Wang <davidlwang2004@gmail.com>
2024-08-30 21:21:51 -0400

    Finished CW2

--- CW-2/index.js
+++ CW-2/index.js
@@ -1,32 +1,38 @@
+const clcBtn = document.getElementById("calc-btn");
+const refreshBtn = document.getElementById("refresh-btn");
+const returnBtn = document.getElementById("return-btn");
+const wakeUpHoursDiv = document.getElementById("wakeup-hours-div");
+const promptSection = document.getElementById("prompt-section");
+const imageContainer = document.getElementById("img-container");
+const resultSection = document.getElementById("result-section");
+
+clcBtn.addEventListener("click", calcWakeUpTimes);
+refreshBtn.addEventListener("click", calcWakeUpTimes);
+returnBtn.addEventListener("click", () => {
+  promptSection.classList.remove("hidden");
+  imageContainer.classList.remove("hidden");
+  resultSection.classList.add("hidden");
+});
+
 function calcWakeUpTimes() {
   const fallAsleepTime = new Date();
   fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
-  console.log(
-    "You will fall sleep at",
-    fallAsleepTime.toLocaleTimeString("en-US", {
-      timeStyle: "short",
-    })
-  );
-
-  const wakeUpTimesDiv = document.getElementById("wakeup-hours-div");
 
   const wakeUpTime = new Date(fallAsleepTime);
-
-  wakeUpTimesDiv.innerHTML = "";
+  wakeUpHoursDiv.innerHTML = "";
   for (let i = 1; i <= 6; i++) {
     wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
     const wakeUpTimeString = wakeUpTime.toLocaleTimeString("en-US", {
       timeStyle: "short",
     });
-
-    const sleepCycleDiv = document.createElement("div");
-    sleepCycleDiv.setAttribute("id", `cycle-${i}`);
-    sleepCycleDiv.classList.add(`cycle`);
-    sleepCycleDiv.textContent = wakeUpTimeString;
-
-    wakeUpTimesDiv.appendChild(sleepCycleDiv);
+    const cycleDiv = document.createElement("div");
+    cycleDiv.classList.add("cycle");
+    cycleDiv.setAttribute("id", `cycle-${i}`);
+    cycleDiv.textContent = wakeUpTimeString;
+    wakeUpHoursDiv.appendChild(cycleDiv);
   }
-}
 
-const calcBtn = document.getElementById("calc-btn");
-calcBtn.onclick = calcWakeUpTimes;
+  promptSection.classList.add("hidden");
+  imageContainer.classList.add("hidden");
+  resultSection.classList.remove("hidden");
+}
